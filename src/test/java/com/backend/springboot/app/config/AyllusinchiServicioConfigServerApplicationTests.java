ab1c681ac980a0c4119903bb08ec1bec821266aa
Gerald <Tu correo>
2022-07-13 19:58:09 -0500

    first commit

---
+++ src/test/java/com/backend/springboot/app/config/AyllusinchiServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.springboot.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyllusinchiServicioConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
